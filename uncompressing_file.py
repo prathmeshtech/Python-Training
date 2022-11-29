5541ee9d692ba50f638a00023e8d289a5a9da132
Shivam Kadukar <shivam.kadukar@cctech.co.in>
2022-11-29 11:52:08 +0530

    adding uncompressing code

--- uncompressing_file.py
+++ uncompressing_file.py
@@ -1,5 +1,14 @@
 import json
 with open('zip.json', 'r') as file:
-    ot = json.load(file)
+    words_dictionary = json.load(file)
 
-print(ot)
+print(words_dictionary)
+
+with open('unzip.txt', 'w') as file:
+    for i in words_dictionary['file']:
+        if i != '*':
+            unzipped_word = list(words_dictionary.keys())[list(words_dictionary.values()).index(int(i))]
+            file.write(unzipped_word)
+        else:
+            file.write('\n')
+
