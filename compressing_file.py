5b879190e915c0366301636dcd51651d1da1f8ae
DESKTOP-10OR5FS\Ganesh Biradar <ganeh.biradar@cctech.co.in>
2022-11-30 09:11:40 +0530

    compressing_file.py

--- compressing_file.py
+++ compressing_file.py
@@ -15,7 +15,6 @@ with open(r'test.txt', 'r') as file:
                 assigned_number += 1
             else:
                 words_dictionary['file'] += str(words_dictionary[word])
-
 print(words_dictionary)
 
 with open('zip.json', 'w') as file:
