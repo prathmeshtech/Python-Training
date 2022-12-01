dbb8966bfb30f00c1d0e74e9de451b1c89f3225b
Prathmesh Singh <prathmeshsingh85@gmail.com>
2022-12-01 10:36:13 +0530

    TC done

--- C_timecomplexity/c_timecomplexity.c
+++ C_timecomplexity/c_timecomplexity.c
@@ -9,10 +9,10 @@ int main(){
     start = clock();
     int res=0;
 
-    for(int i=0; i<1000; i++){
-        for(int j=1; j<1000; j++){
+    for(int i=0; i<20000; i++){
+        for(int j=1; j<20000; j++){
             res = res + j;
-            printf("%d", res);
+            // printf("%d", res);
         }
     }
 
@@ -20,6 +20,6 @@ int main(){
     printf("\n");
     end = clock();
     cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    printf("%lf", cpu_time_used/60);
+    printf("%lf", cpu_time_used);
 
 }
